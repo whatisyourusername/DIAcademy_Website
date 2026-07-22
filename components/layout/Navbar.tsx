c2e029624581fd415bb4ec3f762aa6c23c8a0f93
Yunho Lee <38508401+whatisyourusername@users.noreply.github.com>
2026-07-22 13:17:57 +0900

    Update : Demo_Step_1

--- components/layout/Navbar.tsx
+++ components/layout/Navbar.tsx
@@ -1,17 +1,30 @@
 export default function Navbar() {
   return (
-    <nav className="w-full h-20 flex items-center justify-between px-10 bg-white shadow-sm">
+    <nav className="sticky top-0 z-50 flex h-20 items-center justify-between border-b border-gray-200 bg-white px-8">
 
-      <h1 className="text-2xl font-bold">
+      <h1 className="text-2xl font-bold text-blue-700">
         DI Academy
       </h1>
 
-      <div className="flex gap-8">
-        <button>About</button>
-        <button>Courses</button>
-        <button>Teachers</button>
-        <button>Contact</button>
-      </div>
+      <ul className="flex gap-8 text-gray-700 font-medium">
+
+        <li className="cursor-pointer hover:text-blue-700">
+          About
+        </li>
+
+        <li className="cursor-pointer hover:text-blue-700">
+          Courses
+        </li>
+
+        <li className="cursor-pointer hover:text-blue-700">
+          Teachers
+        </li>
+
+        <li className="cursor-pointer hover:text-blue-700">
+          Contact
+        </li>
+
+      </ul>
 
     </nav>
   );
